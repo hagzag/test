86975c390c37a7c6551a5578d27beeda2bbe9892
Haggai Philip Zagury <hagzag@tikalk.com>
2023-03-05 08:40:33 +0200

    fix: terragorm fmt

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -4,9 +4,9 @@ data "aws_route53_zone" "domain_zone" {
 }
 
 module "acm" {
-  source = "terraform-aws-modules/acm/aws"
+  source  = "terraform-aws-modules/acm/aws"
   version = "4.3.2"
-  
+
   domain_name = data.aws_route53_zone.domain_zone.name
   zone_id     = data.aws_route53_zone.domain_zone.id
 
