a307002895d6cbe799e5ee01dcc4ca645c1c4b09
Haggai Philip Zagury <hagzag@tikalk.com>
2023-03-05 08:35:05 +0200

    add version

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -5,7 +5,8 @@ data "aws_route53_zone" "domain_zone" {
 
 module "acm" {
   source = "terraform-aws-modules/acm/aws"
-
+  version = "4.3.2"
+  
   domain_name = data.aws_route53_zone.domain_zone.name
   zone_id     = data.aws_route53_zone.domain_zone.id
 
